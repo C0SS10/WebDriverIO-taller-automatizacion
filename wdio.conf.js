3ad151cc874bfa13692bfbce38f8801cfdeb6cf3
Esteban <estebancg12@hotmail.com>
2025-06-25 12:16:09 -0500

    Ejecutando workflow 🧪

--- wdio.conf.js
+++ wdio.conf.js
@@ -22,7 +22,7 @@ export const config = {
   // then the current working directory is where your `package.json` resides, so `wdio`
   // will be called from there.
   //
-  specs: ["./features/**/loanrequest.feature"],
+  specs: ["./features/**/login.feature"],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
